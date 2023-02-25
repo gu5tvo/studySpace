18091a7f60f6e13402c7fe517f4956506f07b3ad
gu5tvo <gustavpassinho@gmail.com>
2023-02-25 15:18:09 -0300

    form class complete

--- javaStudySpace/form/src/main/java/form.java
+++ javaStudySpace/form/src/main/java/form.java
@@ -1,1 +1,28 @@
-public class Form {}
+import javax.swing.*;
+import java.awt.Flowlayout;
+
+public class Form extends JFrame {
+  JLabel lName;
+  jTextField tfName;
+  JButton bSave;
+
+  public Form(){
+    lName = new JLabel("Name";)
+    add(tfName);
+
+    tfName = new jTextField(20);
+    add(tfName);
+
+    bSave = new JButton("Save");
+
+    setLayout(new Flowlayout());
+    setSize(800, 600);
+    setVisible(true);
+  }
+
+
+  public static void main(String[] args) {
+    Form f = new Form();
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
